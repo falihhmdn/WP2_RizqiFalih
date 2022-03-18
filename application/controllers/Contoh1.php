fae8758e4b8e89d96290156558480f58c3726a3d
falihhmdn <tugasfalih@gmail.com>
2022-03-18 15:55:46 +0700

    menambahkan .

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perknalkan</h1>";
+        echo" Nama saya Rizqi Falih
+              Saya tinggal di daerah Bekasi
+              olah raga yang saya sukai adalah
+              Berenang";
+    }
+}
